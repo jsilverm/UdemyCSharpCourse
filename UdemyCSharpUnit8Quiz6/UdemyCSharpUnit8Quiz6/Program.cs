ab6586c6bdf5d3bf08ffe0cc7bd893de53520dfb
jsilverm <42704112+jsilverm@users.noreply.github.com>
2019-01-26 16:29:52 -0500

    I didn't think that the provided answer was correct to this quiz problem, and here's why.

---
+++ UdemyCSharpUnit8Quiz6/UdemyCSharpUnit8Quiz6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace UdemyCSharpUnit8Quiz6
+{
+    class Program
+    {
+        void ParentMethod()
+        {
+            Console.WriteLine("Hi from to the parent method");
+            ChildMethod();
+            void ChildMethod()
+            {
+                Console.WriteLine("Hi from the child method");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program pgm = new Program();
+            pgm.ParentMethod();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
